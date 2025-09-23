27ce2a61b5b92cfdf2481d5598c9744017d27d0b
fdcirettg <federico.cirett@unison.mx>
2025-09-22 23:01:16 -0700

    Agreamos campo de texto para nombre de veterinaria a configuración

---
+++ lib/app_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+  const AppText({super.key, required this.text, this.style});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: style ?? Theme.of(context).textTheme.headlineMedium,
+      textAlign: TextAlign.center,
+    );
+  }
+}
--- lib/configuracion_page.dart
+++ lib/configuracion_page.dart
@@ -4,40 +4,72 @@ import 'settings_controller.dart';
 import 'app_scaffold.dart';
 import 'package:provider/provider.dart';
 import 'theme_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import  'dart:io';
+import 'app_text.dart';
 
 class ConfiguracionPage extends StatelessWidget {
   ConfiguracionPage({super.key});
 
   final SettingsController settingsController = Get.put(SettingsController());
+  final TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
+    _loadPersistedData();
     return AppScaffold(
       title: 'Configuración',
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: RadioGroup<ThemeMode>(
-          groupValue: themeProvider.themeMode,
-          onChanged: (mode) => themeProvider.setTheme(mode!),
-          child: Column(
-            children: <Widget>[
-            RadioListTile<ThemeMode>(
-              value: ThemeMode.light,
-              title: Text('Claro'),
-            ),
-            RadioListTile<ThemeMode>(
-              value: ThemeMode.dark,
-              title: Text('Oscuro'),
-            ),
-            RadioListTile<ThemeMode>(
-              value: ThemeMode.system,
-              title: Text('Seguir sistema'),
-            ),
-          ],
-        ),
-      ),
+        child:
+          ListView(
+            children: [ 
+              AppText(text: 'Personaliza tu aplicación', style: Theme.of(context).textTheme.headlineSmall),
+              TextField(
+                controller: textController,
+                  decoration: InputDecoration(
+                    labelText: 'Nombre de veterinaria',
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (value) {
+                  _saveCustomText(value);
+                },
+                ),
+              
+              const SizedBox(height: 20),
+              RadioGroup<ThemeMode>(
+                groupValue: themeProvider.themeMode,
+                onChanged: (mode) => themeProvider.setTheme(mode!),
+                child: Column(
+                  children: <Widget>[
+                  RadioListTile<ThemeMode>(
+                    value: ThemeMode.light,
+                    title: Text('Claro'),
+                  ),
+                  RadioListTile<ThemeMode>(
+                    value: ThemeMode.dark,
+                    title: Text('Oscuro'),
+                  ),
+                  RadioListTile<ThemeMode>(
+                    value: ThemeMode.system,
+                    title: Text('Seguir sistema'),
+                  ),
+                  ],
+                ),
+              ),
+            ],
+          ),
     ),
     );
+  }
+  void _saveCustomText(String text) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('custom_text', text);
+  }
+  void _loadPersistedData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedText = prefs.getString('custom_text') ?? '';
+    textController.text = savedText;
   }
 }
